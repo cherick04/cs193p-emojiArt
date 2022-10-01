c5bdfc504470e8ca04d74019b681280d87f747d6
Erick Chacon <cherickdev@gmail.com>
2022-10-01 12:48:31 -0400

    Set up `EmojiArtDocumentView`

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiArtDocumentView.swift
 //  EmojiArt
 //
 //  Created by Erick Chacon on 10/1/22.
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiArtDocumentView: View {
+    /// ViewModel
+    @ObservedObject var document: EmojiArtDocument
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +24,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        EmojiArtDocumentView(document: EmojiArtDocument() )
     }
 }
