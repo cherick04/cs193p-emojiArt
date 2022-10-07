e067c970054733009258e2af4c77226888e57ad7
Erick Chacon <cherickdev@gmail.com>
2022-10-07 14:24:44 -0400

    Add context menu to palette button to add/delete palettes

--- EmojiArt/UtilityViews.swift
+++ EmojiArt/UtilityViews.swift
@@ -18,3 +18,33 @@ struct OptionalImage: View {
         }
     }
 }
+
+// syntactic sugar
+// lots of times we want a simple button
+// with just text or a label or a systemImage
+// but we want the action it performs to be animated
+// (i.e. withAnimation)
+// this just makes it easy to create such a button
+// and thus cleans up our code
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            if title != nil && systemImage != nil {
+                Label(title!, systemImage: systemImage!)
+            } else if title != nil {
+                Text(title!)
+            } else if systemImage != nil {
+                Image(systemName: systemImage!)
+            }
+        }
+    }
+}
