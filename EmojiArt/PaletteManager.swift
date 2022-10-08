b4d0223678b821a00acb73bd0fda11fbfc5f0dbd
Erick Chacon <cherickdev@gmail.com>
2022-10-08 18:09:29 -0400

    Add edit mode to `PaletteManager`

--- EmojiArt/PaletteManager.swift
+++ EmojiArt/PaletteManager.swift
@@ -10,21 +10,26 @@ import SwiftUI
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
     
+    @State private var editMode: EditMode = .inactive
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.palettes) { palette in
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                         VStack(alignment: .leading) {
-                            Text(palette.name)
+                            Text(palette.name).foregroundColor(editMode == .active ? .red : .black)
                             Text(palette.emojis)
                         }
                     }
-                    
                 }
             }
             .navigationTitle("Manage palettes")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                EditButton()
+            }
+            .environment(\.editMode, $editMode)
         }
     }
 }
